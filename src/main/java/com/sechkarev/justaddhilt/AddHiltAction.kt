16a014f93de680245e336e9145b232477790aed0
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-10-08 15:07:29 +0200

    log all plugins

--- src/main/java/com/sechkarev/justaddhilt/AddHiltAction.kt
+++ src/main/java/com/sechkarev/justaddhilt/AddHiltAction.kt
@@ -1,6 +1,7 @@
 package com.sechkarev.justaddhilt
 
 import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.android.tools.idea.gradle.dsl.api.PluginModel
 import com.android.tools.idea.gradle.dsl.api.ProjectBuildModel
 import com.android.tools.idea.gradle.dsl.model.repositories.MavenCentralRepositoryModel
 import com.intellij.notification.NotificationDisplayType
@@ -12,7 +13,9 @@ import com.intellij.openapi.command.executeCommand
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.psi.codeStyle.CodeStyleManager
+import org.gradle.api.Plugin
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
+import org.jetbrains.kotlin.idea.util.projectStructure.allModules
 
 class AddHiltAction : AnAction() {
 
@@ -21,12 +24,17 @@ class AddHiltAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
 
-        val projectBuildModel = ProjectBuildModel.get(project).projectBuildModel // todo: what does this mean if this is null?
+        val projectBuildModel = ProjectBuildModel.get(project)
+        val projectGradleBuildModel = projectBuildModel.projectBuildModel // todo: what does this mean if this is null?
+        projectBuildModel.allIncludedBuildModels.forEachIndexed { index, it ->
+            logger.warn(index.toString() + " - " + it.moduleRootDirectory.absolutePath + ": " + PluginModel.extractNames(it.plugins()))
+        }
         executeCommand {
             runWriteAction {
-                projectBuildModel?.repositories()?.addRepositoryByMethodName(MavenCentralRepositoryModel.MAVEN_CENTRAL_METHOD_NAME)
-                projectBuildModel?.applyChanges()
-                projectBuildModel?.psiElement?.let { CodeStyleManager.getInstance(project).reformat(it) } // todo: reformat only the changes?.. the entire file might be overkill
+                projectGradleBuildModel?.repositories()?.addRepositoryByMethodName(MavenCentralRepositoryModel.MAVEN_CENTRAL_METHOD_NAME)
+                projectGradleBuildModel?.applyChanges()
+                projectGradleBuildModel?.psiElement?.let { CodeStyleManager.getInstance(project).reformat(it) } // todo: reformat only the changes?.. the entire file might be overkill
+                // todo: sync gradle somehow
             }
         }
     }
