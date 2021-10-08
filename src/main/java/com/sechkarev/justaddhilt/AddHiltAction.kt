121d6143f2674cae6abf505e6e058b7f0d64a855
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-10-08 17:05:36 +0200

    retrieve application names of all modules

--- src/main/java/com/sechkarev/justaddhilt/AddHiltAction.kt
+++ src/main/java/com/sechkarev/justaddhilt/AddHiltAction.kt
@@ -1,8 +1,9 @@
 package com.sechkarev.justaddhilt
 
+import com.android.SdkConstants
 import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
-import com.android.tools.idea.gradle.dsl.api.PluginModel
 import com.android.tools.idea.gradle.dsl.api.ProjectBuildModel
+import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
 import com.android.tools.idea.gradle.dsl.model.repositories.MavenCentralRepositoryModel
 import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationGroup
@@ -12,10 +13,13 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.command.executeCommand
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
+import com.intellij.psi.XmlRecursiveElementVisitor
 import com.intellij.psi.codeStyle.CodeStyleManager
-import org.gradle.api.Plugin
+import com.intellij.psi.xml.XmlTag
+import org.jetbrains.android.dom.manifest.AndroidManifestXmlFile
+import org.jetbrains.android.dom.manifest.getPrimaryManifestXml
+import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
-import org.jetbrains.kotlin.idea.util.projectStructure.allModules
 
 class AddHiltAction : AnAction() {
 
@@ -26,9 +30,19 @@ class AddHiltAction : AnAction() {
 
         val projectBuildModel = ProjectBuildModel.get(project)
         val projectGradleBuildModel = projectBuildModel.projectBuildModel // todo: what does this mean if this is null?
-        projectBuildModel.allIncludedBuildModels.forEachIndexed { index, it ->
-            logger.warn(index.toString() + " - " + it.moduleRootDirectory.absolutePath + ": " + PluginModel.extractNames(it.plugins()))
+//        projectBuildModel.allIncludedBuildModels.forEachIndexed { index, it ->
+//            logger.warn(index.toString() + " - " + it.moduleRootDirectory.absolutePath + ": " + PluginModel.extractNames(it.plugins()))
+//        }
+        val androidBaseBuildModels = projectBuildModel.allIncludedBuildModels.filter { moduleBuildModel ->
+            moduleBuildModel.plugins().any { plugin ->
+                plugin.name().getValue(GradlePropertyModel.STRING_TYPE)?.equals("com.android.application") == true
+            }
         }
+        logger.warn("androidBaseBuildModels: " + androidBaseBuildModels.joinToString { it.moduleRootDirectory.name })
+        val androidFacets = androidBaseBuildModels.mapNotNull { it.psiElement?.let { psiElement -> AndroidFacet.getInstance(psiElement) } }
+        logger.warn("androidFacets: " + androidFacets.joinToString { it.module.name })
+        val applicationNames = androidFacets.map { it.getPrimaryManifestXml()?.findApplicationName() }
+        logger.warn("applicationNames: ${applicationNames.joinToString()}")
         executeCommand {
             runWriteAction {
                 projectGradleBuildModel?.repositories()?.addRepositoryByMethodName(MavenCentralRepositoryModel.MAVEN_CENTRAL_METHOD_NAME)
@@ -57,5 +71,17 @@ class AddHiltAction : AnAction() {
             type = NotificationType.INFORMATION,
             listener = null,
         ).notify(project)
+    }
+
+    private fun AndroidManifestXmlFile.findApplicationName(): String? {
+        var result: String? = null
+        accept(object : XmlRecursiveElementVisitor() {
+            override fun visitXmlTag(tag: XmlTag?) {
+                super.visitXmlTag(tag)
+                if ("application" != tag?.name) return
+                tag.getAttributeValue(SdkConstants.ATTR_NAME, SdkConstants.ANDROID_URI)?.let { result = it }
+            }
+        })
+        return result
     }
 }
