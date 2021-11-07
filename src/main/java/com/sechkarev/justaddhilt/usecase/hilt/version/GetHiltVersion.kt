8f2aa61c14b3da3b3fd98bccf6f3736ac9d06c41
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-11-07 15:03:37 +0100

    extract a little more

--- src/main/java/com/sechkarev/justaddhilt/usecase/hilt/version/GetHiltVersion.kt
+++ src/main/java/com/sechkarev/justaddhilt/usecase/hilt/version/GetHiltVersion.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 
 /*
- todo: might be a good idea to get the fresh version from the internet
+ It might be a good idea to get the fresh version from the internet
  (https://docs.github.com/en/rest/reference/repos#releases),
  but retrieving it will significantly increase plugin's execution time,
  so I'm not sure.
