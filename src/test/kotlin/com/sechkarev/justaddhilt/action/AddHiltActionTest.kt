1aa4ae7b529eef1b01a1a8780d9c5a28964d90af
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-11-13 17:40:12 +0100

    I am far from done, but maybe on the right way

--- src/test/kotlin/com/sechkarev/justaddhilt/action/AddHiltActionTest.kt
+++ src/test/kotlin/com/sechkarev/justaddhilt/action/AddHiltActionTest.kt
@@ -3,6 +3,7 @@ package com.sechkarev.justaddhilt.action
 import ComputeBasePath
 import com.android.tools.idea.gradle.dsl.api.GradleModelProvider
 import com.android.tools.idea.gradle.dsl.model.ProjectBuildModelImpl
+import com.android.tools.idea.gradle.run.create
 import com.intellij.facet.FacetManager
 import com.intellij.facet.impl.FacetUtil
 import com.intellij.javaee.ExternalResourceManagerExImpl
@@ -10,6 +11,12 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.command.executeCommand
 import com.intellij.openapi.diagnostic.DefaultLogger
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.VfsTestUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
@@ -19,11 +26,13 @@ import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.android.facet.AndroidFacetConfiguration
 import org.jetbrains.android.facet.AndroidFacetType
 import org.jetbrains.android.facet.AndroidGradleFacetEditorForIdea
+import org.jetbrains.kotlin.idea.facet.getOrCreateFacet
 import org.jetbrains.kotlin.idea.util.application.runWriteActionInEdt
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.nio.file.Path
 import kotlin.io.path.Path
 
 class AddHiltActionTest : BasePlatformTestCase() {
@@ -36,39 +45,48 @@ class AddHiltActionTest : BasePlatformTestCase() {
         Assertions.assertThat(testDataPath).isNotNull
     }
 
+    override fun getProjectDescriptor() = TestProjectDescriptor()
+
     @Test
     fun testaTest() {
 //        files.forEach {
 //            myFixture.configureFromExistingVirtualFile(myFixture.copyFileToProject(it))
 //        }
-//        val directory = myFixture.copyDirectoryToProject("testCase1", "")
-//
+//        val directory = myFixture.copyDirectoryToProject(testDataFolder, "")
+//        myFixture.configureFromExistingVirtualFile(directory)
 
-//        val androidFacetType = AndroidFacetType()
-//        executeCommand {
-//            runWriteAction {
-//                FacetManager.getInstance(module).addFacet(androidFacetType, androidFacetType.defaultFacetName, null)
-//            }
-//        }
 
-        val list2 = FileUtils.listFiles(File(testDataPath, "testCase1"), arrayOf("kt", "java", "xml", "gradle"), true).map { it.path }
-        val psiFiles = myFixture.configureByFiles(*list2.toTypedArray())
+        val list2 = FileUtils.listFiles(
+            File(testDataPath, testDataFolder),
+            arrayOf("kt", "java", "xml", "gradle", "gitignore", "pro", "properties"),
+            true
+        ).map { it.path }
+        list2.forEach { myFixture.configureByFile(it) }
+//        val psiFiles = myFixture.configureByFiles(*list2.toTypedArray())
 
-//        PlatformTestUtil.loadAndOpenProject(Path(testDataPath, "testCase1")) { }
+//        PlatformTestUtil.loadAndOpenProject(Path(testDataPath, testDataFolder)) { }
 
 //        val buildModel = GradleModelProvider.getInstance().getProjectModel(project)
 //        val parsedBuildModel = GradleModelProvider.getInstance().parseBuildFile(psiFiles[9].virtualFile, project)
 
         val result = myFixture.testAction(AddHiltAction()) // no android modules present, what a pity
 
-        myFixture.checkResult(files[0], "aaa nu kak tak", true)
+        myFixture.checkResult("aaa nu kak tak", true)
     }
 
     companion object {
+        const val testDataFolder = "EmptyTestApp"
+
         val files = arrayOf(
-            "testCase1/app5/build.gradle",
-            "testCase1/app5/src/main/AndroidManifest.xml",
-            "testCase1/app5/src/main/java/com/example/app5/CustomApplication.kt",
+            "$testDataFolder/app/build.gradle",
+            "$testDataFolder/app/src/main/AndroidManifest.xml",
+            "$testDataFolder/app/src/main/java/com/example/app5/CustomApplication.kt",
         )
+    }
+
+    class TestProjectDescriptor : LightProjectDescriptor() {
+        override fun configureModule(module: Module, model: ModifiableRootModel, contentEntry: ContentEntry) {
+
+        }
     }
 }
