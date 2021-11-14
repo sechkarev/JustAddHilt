378ef33f0e7ed8c38eb0827a0f3553bb532323a7
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-11-14 12:08:07 +0100

    cleanup build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.31"
     id("org.jetbrains.intellij") version "1.2.0"
     java
 }
 
 group = "com.sechkarev"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -14,7 +14,6 @@ repositories {
 dependencies {
     implementation("org.freemarker:freemarker:2.3.31")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
-    testImplementation("org.assertj:assertj-core:3.11.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
