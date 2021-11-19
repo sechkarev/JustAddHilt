a7c4bfed645d93c8d6d1a310c2143fa650199c96
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-11-19 19:51:18 +0100

    format imports in java classes

--- src/main/java/com/sechkarev/justaddhilt/usecases/hilt/annotation/AddHiltAnnotationToJavaClass.kt
+++ src/main/java/com/sechkarev/justaddhilt/usecases/hilt/annotation/AddHiltAnnotationToJavaClass.kt
@@ -3,8 +3,10 @@ package com.sechkarev.justaddhilt.usecases.hilt.annotation
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiImportList
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.codeStyle.JavaCodeStyleManager
+import com.intellij.psi.util.PsiTreeUtil
 
 @Service
 class AddHiltAnnotationToJavaClass(private val project: Project) {
@@ -15,5 +17,8 @@ class AddHiltAnnotationToJavaClass(private val project: Project) {
             ?.addAnnotation("dagger.hilt.android.HiltAndroidApp")
         addedAnnotation?.let { JavaCodeStyleManager.getInstance(project).shortenClassReferences(it) }
         addedAnnotation?.parent?.let { CodeStyleManager.getInstance(project).reformat(it) }
+        PsiTreeUtil.findChildOfType(addedAnnotation?.containingFile, PsiImportList::class.java)?.let {
+            CodeStyleManager.getInstance(project).reformat(it)
+        }
     }
 }
