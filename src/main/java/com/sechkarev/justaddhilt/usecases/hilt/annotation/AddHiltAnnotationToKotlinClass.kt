12b8aa876183b5a9eed54a835e4670376d5fee8c
Sechkarev <vladimir.sechkarev@excelsior-solutions.de>
2021-11-20 14:42:54 +0100

    try to add annotation to kotlin class the right way

--- src/main/java/com/sechkarev/justaddhilt/usecases/hilt/annotation/AddHiltAnnotationToKotlinClass.kt
+++ src/main/java/com/sechkarev/justaddhilt/usecases/hilt/annotation/AddHiltAnnotationToKotlinClass.kt
@@ -4,7 +4,11 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.refactoring.suggested.startOffset
+import com.intellij.util.FileContentUtil
+import org.jetbrains.kotlin.idea.core.ShortenReferences
+import org.jetbrains.kotlin.psi.KtFile
 
 /*
  Unfortunately, we can't add an annotation to a Kotlin class via PSI tree as we did to a Java class
@@ -17,6 +21,15 @@ class AddHiltAnnotationToKotlinClass(private val project: Project) {
     operator fun invoke(psiClass: PsiClass) {
         val document = PsiDocumentManager.getInstance(project).getDocument(psiClass.containingFile)
         document?.insertString(psiClass.startOffset, "@dagger.hilt.android.HiltAndroidApp\n")
-        // todo: shorten class references and reformat (do I need to recreate PSI tree for that?)
+        FileContentUtil.reparseFiles(
+            project,
+            listOf(psiClass.containingFile.virtualFile),
+            false,
+        )
+        // fixme: the KtFile does not contain the new annotation
+        document
+            ?.let { PsiDocumentManager.getInstance(project).getPsiFile(it) as KtFile }
+            ?.let { ShortenReferences.DEFAULT.process(it) }
+        psiClass.containingFile?.let { CodeStyleManager.getInstance(project).reformat(it) }
     }
 }
